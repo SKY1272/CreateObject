f9bc8c7f1188bc8ef194795b0de30dfb11990b99
SKY <88556477+SKY1272@users.noreply.github.com>
2023-10-30 01:56:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import CreateObject from "./CreateObject";
+export default function App() {
+  return (
+    <div className="App">
+      <CreateObject />
+    </div>
+  );
+}
